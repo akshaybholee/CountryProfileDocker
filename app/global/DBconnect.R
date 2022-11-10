4af676ea926aa8cc6e1fa110082db3be540f8314
Akshay Bholee <akshay.bholee@gmail.com>
2022-11-10 13:14:48 +0400

    db test 6

--- app/global/DBconnect.R
+++ app/global/DBconnect.R
@@ -17,7 +17,7 @@ url <- httr::parse_url(Sys.getenv("DATABASE_URL"))
 #               port = 1433)
 
 con <- dbPool(odbc(),
-              Driver = DBI::dbDriver("FreeTDS"),
+              Driver = "ODBC Driver 17 for SQL Server",
               Server = url$hostname,
               Database = url$path,
               UID = url$user,
