e29635af04a1394c885eb7f32939dd6ce32e7cb2
Akshay Bholee <akshay.bholee@gmail.com>
2022-11-10 15:15:46 +0400

    db test

--- app/global/DBconnect.R
+++ app/global/DBconnect.R
@@ -16,11 +16,15 @@ url <- httr::parse_url(Sys.getenv("DATABASE_URL"))
 #               PWD = "International123",
 #               port = 1433)
 
-con <- dbPool(odbc::odbc(),
-              Driver = "psqlodbcw",
-              Server = url$hostname,
-              Database = url$path,
-              UID = url$user,
-              PWD = url$password,
-              port = url$port)
+# con <- dbPool(odbc::odbc(),
+#               Driver = "psqlodbcw",
+#               Server = url$hostname,
+#               Database = url$path,
+#               UID = url$user,
+#               PWD = url$password,
+#               port = url$port)
+
+
+
+con <- odbcDriverConnect(connection = "Driver=FreeTDS;TDS_Version=8.0;Server=iecproduction.database.windows.net;Port=1433;Database=IECPRODDB;Uid=IECPROD;Pwd=International123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 
