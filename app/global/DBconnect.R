1e3d2e865c6ca81326d8a7fb17759d990e20ab36
Akshay Bholee <akshay.bholee@gmail.com>
2022-11-11 12:58:43 +0400

    db driver

--- app/global/DBconnect.R
+++ app/global/DBconnect.R
@@ -10,7 +10,7 @@
 print(odbc::odbcListDrivers())
 
 con <- dbPool(odbc(),
-              Driver = "SQLServer",
+              Driver = "ODBC Driver 17 for SQL Server",
               Server = "iecproduction.database.windows.net",
               Database = "IECPRODDB",
               UID = "IECPROD",
