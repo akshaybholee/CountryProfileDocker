5040cea54786a1120947502d9d6a37ff00f597c3
Akshay Bholee <akshay.bholee@gmail.com>
2022-11-11 10:04:13 +0400

    db test 15

--- app/global/DBconnect.R
+++ app/global/DBconnect.R
@@ -9,7 +9,7 @@
 # use this connection when deploying application to shinyapps.io
 
 con <- dbPool(odbc(),
-              Driver = "ODBC Driver 17.5 for SQL Server",
+              Driver = "ODBC Driver 17 for SQL Server",
               Server = "iecproduction.database.windows.net",
               Database = "IECPRODDB",
               UID = "IECPROD",
