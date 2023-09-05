58178a5aa4dd0da750c0d321ca6f6bf2e3d2a3e4
Akshay Bholee <akshay.bholee@gmail.com>
2023-09-05 17:12:43 +0400

    DB

--- app/global/DBconnect.R
+++ app/global/DBconnect.R
@@ -1,21 +1,21 @@
 # use this connection when running application on local machine
-# con <- dbPool(odbc(),
-#               Driver = "SQL Server",
-#               Server = "iecproduction.database.windows.net",
-#               Database = "IECPRODDB",
-#               UID = "IECPROD",
-#               PWD = "International123")
-
-# use this connection when deploying application to shinyapps.io
-print(odbc::odbcListDrivers())
-
 con <- dbPool(odbc(),
-              Driver = "ODBC Driver 17 for SQL Server",
+              Driver = "SQL Server",
               Server = "iecproduction.database.windows.net",
               Database = "IECPRODDB",
               UID = "IECPROD",
-              PWD = "International123",
-              port = 1433)
+              PWD = "International123")
+
+# use this connection when deploying application to shinyapps.io
+# print(odbc::odbcListDrivers())
+# 
+# con <- dbPool(odbc(),
+#               Driver = "ODBC Driver 17 for SQL Server",
+#               Server = "iecproduction.database.windows.net",
+#               Database = "IECPRODDB",
+#               UID = "IECPROD",
+#               PWD = "International123",
+#               port = 1433)
 
 # con <- dbPool(odbc::odbc(),
 #               Driver = "psqlodbcw",
